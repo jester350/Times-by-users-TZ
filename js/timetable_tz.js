0b45f218ada18143df29b941bf02e6dee0dc0965
jester350 <jester350@gmail.com>
2023-06-05 16:41:06 +0100

    all working

---
+++ js/timetable_tz.js
@@ -0,0 +1,48 @@
+const eventsData = JSON.parse(document.getElementById("events-data").textContent);
+
+function displayLocalTime(eventTime) {
+  var userLocale = navigator.language || navigator.userLanguage;
+  const tzDiff = document.getElementById("eventtz").value
+  const tts = new Date(eventTime);
+  const localTimeString = new Date(tts.getTime() + tzDiff * 60 * 60 * 1000).toLocaleString(userLocale);
+  const currentDate = new Date();
+  const formattedDate = new Intl.DateTimeFormat(navigator.language, {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZoneName: 'short'
+  }).format(currentDate);
+  localTimeString.textContent = formattedDate;	
+  return localTimeString;
+}
+
+function getUserTimezone() {
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    return timeZone;
+}
+
+function displayUserTimezone() {
+    const timezoneElement = document.getElementById("timezone");
+    const timezone = getUserTimezone();
+    timezoneElement.textContent = timezone;
+}
+
+function createTimetable() {
+  const timetable = document.getElementById("timetable");
+  const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  eventsData.forEach((event) => {
+    const row = timetable.insertRow(-1);
+    const eventNameCell = row.insertCell(0);
+    const eventTimeCellStart = row.insertCell(1);
+    const eventTimeCellEnd = row.insertCell(2);
+    eventNameCell.textContent = event.name;
+    eventTimeCellStart.textContent = displayLocalTime(event.stime);
+    eventTimeCellEnd.textContent = displayLocalTime(event.etime);
+  });
+  displayUserTimezone();
+}
+
+document.addEventListener("DOMContentLoaded", createTimetable);
